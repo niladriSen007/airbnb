6ae6e4b9236af5599c51307ea8fbf7974e92eb20
Niladri <niladrisengit12@gmail.com>
2023-11-05 20:11:36 +0530

    day-02

--- pages/components/CityList.jsx
+++ pages/components/CityList.jsx
@@ -1,53 +1,217 @@
 import React, { useState } from "react";
-import { BiSolidUpArrow,BiSolidDownArrow } from "react-icons/bi";
+import { BiSolidUpArrow, BiSolidDownArrow } from "react-icons/bi";
 
 const cities = [
-  {id:1,city:"Bangalore",areas:[],isDropDown:false},
-  {id:2,city:"Chennai",areas:[],isDropDown:false},
-  {id:3,city:"Delhi",areas:[],isDropDown:false},
-  {id:4,city:"Gurgaon",areas:[],isDropDown:false},
-  {id:5,city:"Hyderabad",areas:[],isDropDown:false},
-  {id:6,city:"Kolkata",areas:[],isDropDown:false},
-  {id:7,city:"Mumbai",areas:[],isDropDown:false},
-  {id:8,city:"Noida",areas:[],isDropDown:false},
-  {id:9,city:"NoidPunea",areas:[],isDropDown:false},
+  {
+    id: 1,
+    city: "Bangalore",
+    areas: [,
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 2,
+    city: "Chennai",
+    areas: [
+      "Chennai Central Railway Station",
+      "T Nagar",
+      "Ecr East Coast Road",
+      "Koyambedu",
+      "Mount Road",
+      "Ramapuram",
+      "Porur",
+      "Majestic",
+      "Chennai Egmore Railway Station",
+      "Anna Nagar",
+      "Velachery",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 3,
+    city: "Delhi",
+    areas: [
+      "Paharganj",
+      "Karol Bagh",
+      "Janakpuri",
+      "Dwarka, New Delhi",
+      "Mahipalpur",
+      "Indira Gandhi International Airport",
+      "Saket",
+      "Lajpat Nagar",
+      "New Delhi Railway Station",
+      "Rohini",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 4,
+    city: "Gurgaon",
+    areas: [
+      "Popular Localities",
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 5,
+    city: "Hyderabad",
+    areas: [
+      "Popular Localities",
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 6,
+    city: "Kolkata",
+    areas: [
+      "Popular Localities",
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 7,
+    city: "Mumbai",
+    areas: [
+      "Popular Localities",
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 8,
+    city: "Noida",
+    areas: [
+      "Popular Localities",
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
+  {
+    id: 9,
+    city: "Pune",
+    areas: [
+      "Popular Localities",
+      "Koramangala",
+      "Mg Road",
+      "Rajaji Nagar",
+      "Indiranagar",
+      "Jayanagar",
+      "Madiwala",
+      "Majestic",
+      "Yesvantpur Railway Station",
+      "Marathahalli",
+      "Hsr Layout",
+    ],
+    isDropDown: false,
+  },
 ];
 
-
-
-
 const CityList = () => {
-
-
-  const [items,setItems] = useState(cities)
+  const [items, setItems] = useState(cities);
   const [dd, setDd] = useState(false);
 
   const toggleDropdown = (itemId) => {
     setItems((prevItems) =>
-    prevItems.map((item) =>
-    item.id === itemId ? { ...item, isDropDown: !item.isDropDown } : item
-    )
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, isDropDown: !item.isDropDown } : { ...item, isDropDown: false }
+      )
     );
   };
 
   return (
-    <div className="flex items-center justify-between px-16 h-6  bg-violet-200">
+    <div className="flex items-center justify-between px-16 h-6  bg-red-200">
       {items.map((city, index) => {
         return (
           <div
             key={city?.id}
-            className="flex items-center gap-1 cursor-pointer text-black"
+            className="flex items-center gap-1 cursor-pointer text-black relative"
             // onMouseOver={() => setDd(!dd)}
-            onClick={()=>toggleDropdown(city?.id)}
+            onClick={() => toggleDropdown(city?.id)}
           >
             <span>{city?.city}</span>
             {city?.isDropDown ? (
-              <span className="text-lg font-bold"><BiSolidUpArrow color="purple"/></span>
+              <span className="text-lg font-bold">
+                <BiSolidUpArrow color="red" />
+              </span>
             ) : (
               <span>
-                <BiSolidDownArrow color="purple"/>
+                <BiSolidDownArrow color="red" />
               </span>
             )}
+            {
+              city?.isDropDown && (
+                <div className="flex flex-col gap-2 items-start shadow-xl  absolute top-6 p-2 py-4 w-36  z-40 bg-white">
+                    {
+                      city?.areas?.map((c,index)=>{
+                        return (
+                          <div key={index} className="cursor-pointer ">
+                              <span className= "text-sm hover:text-black hover:font-bold transition-all duration-300">{c}</span>
+                          </div>
+                        )
+                      })
+                    }
+                </div>
+              )
+            }
           </div>
         );
       })}
--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -44,7 +44,7 @@ const navbarMenu = [
 
 const Navbar = ({showLoginForm, setShowLoginForm}) => {
   return (
-    <nav  className="relative flex items-center justify-between h-20 bg-gradient-to-l from-slate-900 to-violet-950 px-6 line-clamp-2">
+    <nav  className="relative flex items-center justify-between h-20 bg-white px-6 line-clamp-2">
       <div>
         <Image src={logo} width={140} height={140} alt="Airbnb" />
       </div>
@@ -54,21 +54,21 @@ const Navbar = ({showLoginForm, setShowLoginForm}) => {
             <div key={id} className="flex items-center gap-2 cursor-pointer">
               <div>
                 <h1 className="cursor-pointer">
-                  <nm.icon size={24} color="white" />
+                  <nm.icon size={24} color="red" />
                 </h1>
               </div>
               <div className="flex flex-col justify-center -gap-4">
-                <span className="text-sm text-white font-extrabold">
+                <span className="text-sm text-black font-extrabold">
                   {nm?.title}
                 </span>
-                <span className="text-xs text-gray-100">{nm?.desc}</span>
+                <span className="text-xs text-gray-800">{nm?.desc}</span>
               </div>
             </div>
           );
         })}
         <button
           onClick={()=>setShowLoginForm(!showLoginForm)}
-          className="bg-violet-600 hover:bg-violet-900 hover:font-semibold text-white px-4 py-1 rounded-md transition-all duration-300 ml-10 drop-shadow-xl"
+          className="bg-red-500 hover:bg-red-700 hover:font-semibold text-white px-4 py-1 rounded-md transition-all duration-300 ml-10 drop-shadow-xl"
         >
           Login/Sign up
         </button>
